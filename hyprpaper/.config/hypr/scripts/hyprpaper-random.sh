13a8d7b1302be0d488afd5a01719d10b6ca1b339
Guilherme Augusto de Macedo <gaugustomacedo@gmail.com>
2025-10-03 19:41:32 -0300

    Personalizando vários componentes

--- hyprpaper/.config/hypr/scripts/hyprpaper-random.sh
+++ hyprpaper/.config/hypr/scripts/hyprpaper-random.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-WALLPAPER_DIR="$HOME/.config/hypr/wallpapers/"
+WALLPAPER_DIR="$HOME/.config/backgrounds/"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 # Get a random wallpaper that is not the current one
