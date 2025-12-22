5d3bb8ac205f41b20344624f7d34aa09dd1355da
Guilherme Augusto de Macedo <gaugustomacedo@gmail.com>
2025-12-22 17:13:19 -0300

    matugen

--- wallset/.config/hypr/scripts/wallset.sh
+++ wallset/.config/hypr/scripts/wallset.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 dir="$HOME/.config/rofi/launchers/type-1"
 theme='style-6'
@@ -14,10 +14,8 @@ IFS=$'\n'
 
 SELECTED_WALL=$(for a in *.jpg *.png; do echo -en "$a\0icon\x1f$a\n"; done | rofi -dmenu -p -theme ${dir}/${theme}.rasi "")
 
-echo "$WALL_DIR/$SELECTED_WALL"
-
 if [ -n "$SELECTED_WALL" ]; then
-  wallset-backend.sh "$WALL_DIR/$SELECTED_WALL"
+  $HOME/.local/bin/wallset-backend.sh "$WALL_DIR/$SELECTED_WALL"
 fi
 
 cd "$CWD" || exit
