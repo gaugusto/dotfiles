97605d9761ec451d65dc4d87f18e801bec2855e5
= <gaugustomacedo@gmail.com>
2025-12-30 14:48:36 -0300

    cliphist minor fix

--- scripts/.local/bin/cliphist_rofi.sh
+++ scripts/.local/bin/cliphist_rofi.sh
@@ -3,4 +3,4 @@
 rofi_launcher="$HOME/.config/rofi/launchers/type-2"
 rofi_theme='style-1'
 
-cliphist list | rofi -dmenu -theme ${rofi_launcher}/${rofi_theme}.rasi | cliphist decode | wl-copy
+cliphist list | rofi -dmenu -p "" -theme ${rofi_launcher}/${rofi_theme}.rasi | cliphist decode | wl-copy
