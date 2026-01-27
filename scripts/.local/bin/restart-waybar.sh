e62b542eff8a0282f238ec0d8047cdd352c43413
Guilherme Augusto de Macedo <gaugustomacedo@gmail.com>
2026-01-27 20:58:20 -0300

    some much changes

--- scripts/.local/bin/restart-waybar.sh
+++ scripts/.local/bin/restart-waybar.sh
@@ -4,13 +4,3 @@ pkill -SIGUSR2 waybar
 
 swaync-client --reload-config
 swaync-client --reload-css
-
-# killall -9 waybar
-# killall -9 swaync
-#
-# sleep 1
-#
-# waybar &
-# -disown
-# swaync &
-# -disown
